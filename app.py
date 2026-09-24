c06b0321cd6afe61a8ad247974f86405210e3115
AreldoA <alex.apolo@mutualistapichincha.com>
2026-09-23 21:06:59 -0500

    Implement file uploader for CSV input
    Add file uploader to load CSV files and display data

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ import streamlit as st
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Alex Apolo")
+archivo = st.file_uploader("Cargue su archivo")
+ 
+
+if archivo is not None:
+
+  tabla = pd.read_csv(archivo)
+
+  st.write(tabla)
