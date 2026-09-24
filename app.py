f5e60bfd4b69f0a5b13c1dacc403d73c77f566c2
AreldoA <alex.apolo@mutualistapichincha.com>
2026-09-23 21:12:42 -0500

    Fix formatting of file uploader and CSV loading

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import streamlit as st
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Alex Apolo")
-archivo = st.file_uploader("Cargue su archivo")
-  
+archivo = st.file_uploader("Cargue su archivo")  
 
 if archivo is not None:
 
-  tabla = pd.read_csv(archivo)
+tabla = pd.read_csv(archivo)
 
   st.write(tabla)
