254dbc366badd61cbda9bb3b53e87a7e15f01cb3
AreldoA <alex.apolo@mutualistapichincha.com>
2026-09-23 21:15:13 -0500

    Refactor author display and file uploader section

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parámetros")
-st.write("Elaborado por: Alex Apolo")
-archivo = st.file_uploader("Cargue su archivo")  
+st.write("Elaborado por: ")
+
+
+archivo = st.file_uploader("Cargue su archivo")
 
 if archivo is not None:
 
-tabla = pd.read_csv(archivo)
+  tabla = pd.read_csv(archivo)
 
   st.write(tabla)
